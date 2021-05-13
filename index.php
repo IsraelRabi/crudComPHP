96112cacd0d7409df0aa6584264113516fee82f5
Israel Rabi <israelrabi01@gmail.com>
2021-05-13 12:23:30 -0300

    adicionei o input name

--- index.php
+++ index.php
@@ -30,6 +30,7 @@
             <div class="form-register" style="display: none;">
                 <form action="phpAction/create.php" method="POST">
                     <input type="text" name="user" placeholder="User" required>
+                    <input type="text" name="name" placeholder="Name" required>
                     <input type="email" name="email" placeholder="Email" required>
                     <input type="password" name="password" placeholder="Password" required>
                     <button type="submit" name="btn-register">Register</button>
