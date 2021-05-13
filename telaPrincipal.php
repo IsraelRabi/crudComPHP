63a218279eff0b21b0d19ff007ac6644bd813f69
Israel Rabi <israelrabi01@gmail.com>
2021-05-13 14:12:51 -0300

    mais um commit

--- telaPrincipal.php
+++ telaPrincipal.php
@@ -1,15 +1,36 @@
 <!DOCTYPE html>
 <html lang="pt-br">
-    
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Bem-vindo <?php echo "opa"; ?></title>
+    <link rel="stylesheet" href="css/telaPrincipal.css" />
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet">
 </head>
 
 <body>
-    
+    <div class="container">
+        <div class="bem-vindo">
+            <p>Seja bem-vindo</p>
+            <p><?php echo "opa"; ?></p>
+        </div>
+        <div class="infos">
+            <p>Usuário: <?php echo "opazinho"; ?></p>
+            <p>Email: <?php echo "opa@gmail.com"; ?></p>
+        </div>
+        <div class="btns">
+            <div class="btn-sair">Sair</div>
+            <div class="btn-editar">Editar</div>
+            <div class="btn-deletar">Deletar</div>
+        </div>
+    </div>
+
+    <script>
+        
+    </script>
 </body>
 
 </html>
