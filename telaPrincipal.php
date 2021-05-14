3e9d215d893490c2abf21a69e4c35385327b2218
Israel Rabi <israelrabi01@gmail.com>
2021-05-14 11:19:47 -0300

    terminei as telas

--- telaPrincipal.php
+++ telaPrincipal.php
@@ -12,9 +12,34 @@
 </head>
 
 <body>
-    <div class="container">
+    <div style="display:none;" class="tem-certeza">
+        <p>Tem certeza que deseja deletar?</p>
+        <div class="certeza-opt">
+            <div class="certeza-opt-sim">
+                <form class="form-deletar" action="phpAction/delete.php" method="POST">
+                    <input type="hidden" name="id" value="<?php echo "lkjsadlfjsdkl"; ?>">
+                    <div class="certeza-opt-sim">SIM</div>
+                </form>
+            </div>
+            <div class="certeza-opt-nao">NÃO</div>
+        </div>
+    </div>
+
+    <div class="editar">
+        <div class="form-register">
+            <form action="phpAction/update.php" method="POST">
+                <input type="text" name="usuario" placeholder="Usuário:" value="<?php echo "teste";?>" required>
+                <input type="text" name="nome" placeholder="Nome:" required>
+                <input type="email" name="email" placeholder="Email:" required>
+                <button type="submit" name="btn-alterar">Alterar</button>
+            </form>
+        </div>
+        <div class="btn-voltar">Voltar</div>
+    </div>
+
+    <div style="display:none;" class="container">
         <div class="bem-vindo">
-            <p>Seja bem-vindo</p>
+            <h1>Seja bem-vindo</h1>
             <p><?php echo "opa"; ?></p>
         </div>
         <div class="infos">
@@ -29,7 +54,41 @@
     </div>
 
     <script>
-        
+        const btnSair = document.querySelector(".btn-sair")
+        const btnEditar = document.querySelector(".btn-editar")
+        const btnDeletar = document.querySelector(".btn-deletar")
+
+        const editar = document.querySelector(".editar")
+        const voltar = document.querySelector(".btn-voltar")
+
+        const formDeletar = document.querySelector(".form-deletar")
+        const delSim = document.querySelector(".certeza-opt-sim")
+        const delNao = document.querySelector(".certeza-opt-nao")
+        const container = document.querySelector(".container")
+        const temCerteza = document.querySelector(".tem-certeza")
+
+        btnSair.addEventListener("click", () => location.href = "phpAction/sair.php")
+        btnEditar.addEventListener("click", () => {
+            editar.style.display = "block";
+            container.style.display = "none";
+        })
+        btnDeletar.addEventListener("click", () => {
+            container.style.display = "none";
+            temCerteza.style.display = "block";
+        })
+
+        voltar.addEventListener("click", () => {
+            editar.style.display = "none"
+            container.style.display = "block";
+        })
+
+        delSim.addEventListener("click", () => {
+            formDeletar.submit()
+        })
+        delNao.addEventListener("click", () => {
+            temCerteza.style.display = "none";
+            container.style.display = "block";
+        })
     </script>
 </body>
 
